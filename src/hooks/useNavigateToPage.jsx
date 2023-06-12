1ce059dbc34bdf6a7f49dd72967554dbedb14762
youseonh <dbtjs0221@gmail.com>
2023-06-12 16:46:48 +0900

    fix: NavigateToPage Hook에서 토큰 확인해서 /todo로 이동하는 코드 추가 close #1

--- src/hooks/useNavigateToPage.jsx
+++ src/hooks/useNavigateToPage.jsx
@@ -1,10 +1,22 @@
 import { useNavigate } from 'react-router-dom'
+import { useContext, useEffect, useCallback } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 export default function useNavigateToPage() {
+  const { token } = useContext(AuthContext)
   const navigate = useNavigate()
-  const navigateToPage = path => {
-    navigate(`/${path}`)
-  }
+  const navigateToPage = useCallback(
+    path => {
+      navigate(`/${path}`)
+    },
+    [navigate],
+  )
+
+  useEffect(() => {
+    if (token && token !== '') {
+      navigateToPage('todo')
+    }
+  }, [navigateToPage, token])
 
   return navigateToPage
 }
